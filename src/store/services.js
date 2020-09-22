b4c8215b27cac2c9ca86add54cf71d662d5e5e13
Crowdbotics <team@crowdbotics.com>
2020-09-22 14:19:09 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,5 +1,10 @@
 import axios from "axios"
 import { NEW_CONNECTOR_GFGFGHF_TOKEN } from "react-native-dotenv"
+const newConnectordgfdgg = axios.create({
+  baseURL:
+    "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/11433/storyboard/11048/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 const newConnectorgfgfghf = axios.create({
   baseURL:
     "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/11240/storyboard/10919/",
